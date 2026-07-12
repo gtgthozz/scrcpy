e7fdb00a2f2c78b2dbb35a741f2ed21403efa032
gtgthozz <44411037+gtgthozz@users.noreply.github.com>
2026-07-13 02:34:34 +0300

    Update release.sh

--- release/release.sh
+++ release/release.sh
@@ -6,19 +6,19 @@ set -e
 cd "$(dirname ${BASH_SOURCE[0]})"
 rm -rf output
 
-./test_server.sh
+# ./test_server.sh
 ./test_client.sh
 
-./build_server.sh
-./build_windows.sh 32
+# ./build_server.sh
+# ./build_windows.sh 32
 ./build_windows.sh 64
-./build_linux.sh x86_64
+# ./build_linux.sh x86_64
 
-./package_server.sh
-./package_client.sh win32 zip
+# ./package_server.sh
+# ./package_client.sh win32 zip
 ./package_client.sh win64 zip
-./package_client.sh linux-x86_64 tar.gz
+# ./package_client.sh linux-x86_64 tar.gz
 
-./generate_checksums.sh
+# ./generate_checksums.sh
 
 echo "Release generated in $PWD/output"
